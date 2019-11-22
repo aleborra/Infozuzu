90f7afb72e56d763715f20dd58a95b9d04ed9539
aleborra <56735829+aleborra@users.noreply.github.com>
2019-11-22 09:39:34 +0100

    Update zuzu.cpp

--- zuzu.cpp
+++ zuzu.cpp
@@ -1,19 +1,28 @@
 #include <stdio.h>
 #include<string.h>
+#include<unistd.h>
 #define MAX 100
 
 int main () {
 	
 	char z[MAX];
 	char *p;
+	int x,i;
 	
-	printf("Inserire una domanda:\n");
+	printf("\nInserire una domanda:\n");
 	gets(z);
 	
 	p=strstr(z,"zuzu");
-  
-  	if(p==NULL)
-  		printf("\n\n\n\n\n\nZUZU.EXE HA SMESSO DI FUNZIONARE\n\n\n\n\n\n\n\n\n");
-  	else
-  		printf("\n\n\n\n\n\nFILE NOT FOUND (404)\n\n\n\n\n\n\n\n\n\n");
+	
+	printf("\nOgni quanto riprovare la ricerca?\n");
+	scanf("%d",&x);
+	
+	for(i=0;i<1000;i++)
+	{
+		sleep(x);
+		if(p==NULL)
+  			printf("\n\n\n\n\n\nZUZU.EXE HA SMESSO DI FUNZIONARE\n\n\n\n\n\n\n");
+  		else
+  			printf("\n\n\n\nFILE NOT FOUND (404)\n\n\n\n\n\n\n\n");
+	}
 }
